5637c03b98fab6bb60eaaa01c860ffb7f34cfc1c
Zac Blanco <zac.blanco@rutgers.edu>
2018-02-22 10:07:42 -0500

    [Rec5] Fix ex3

--- recitations/rec5/ex3.c
+++ recitations/rec5/ex3.c
@@ -14,19 +14,6 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &my_id);     /* get current process id */
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs); /* get number of processes */
 
-    /* proc 0 initializes the principal array */
-    if (my_id == 0)
-    {
-        for (i = 0; i < N; i++)
-            array[i] = i;
-    }
-
-    num_elem = N / num_procs;
-    if (my_id < (N % num_procs))
-        num_elem = num_elem + 1;
-
-    array_recv = (int *)malloc(num_elem * sizeof(int));
-
     // Write the code to complete the exercise below.
 
 
